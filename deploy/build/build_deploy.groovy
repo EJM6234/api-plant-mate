054ad713fa3be40d8ad0701dbec56c73aefadaed
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2020-01-21 22:04:29 -0500

    Configuring build script

--- deploy/build/build_deploy.groovy
+++ deploy/build/build_deploy.groovy
@@ -1,13 +1,5 @@
 #!groovy
 
-node('master') {
-  stage('Setup Environment') {
-    ENVIRONMENT_NAME = 'dev'
-    AWS_REGION = 'us-east-1'
-    APPLICATION_CONTEXT = 'plantmate'
-  }
-}
-
 pipeline {
   agent { docker { image 'ubuntu:latest' } }
 
